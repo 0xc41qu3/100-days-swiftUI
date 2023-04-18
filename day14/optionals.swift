6633ebc8f2e0c321882f2fd41a5de18cfc5f1bf0
Caique M. Oliveira <caique_m_oliveira@icloud.com>
2023-04-18 12:51:22 -0300

    add notes about optionals

---
+++ day14/optionals.swift
@@ -0,0 +1,26 @@
+//Optionals
+let stateByCity = [
+    "são paulo" : "SP",
+    "garulhos": "SP",
+    "campinas": "SP"
+]
+
+let chosenCity = "garuhlos" // ⚠️ There's a typo.
+
+//If we can unwrap the data successfully, the if statement body will run.
+if let stateOfChosenCity = stateByCity[chosenCity] {
+    print("The state of the chosen city is \"\(stateOfChosenCity)\".")
+} else {
+    print("No city has been chosen city.")
+}
+
+//`nill` represent a valueless state.
+var days: Int? = nil
+
+//The constant with the same name is allowed since it only belongs to the if statement's scope.
+//This technique is called shadowing.
+if let days = days {
+    print("Wil be off for \(days) days.")
+} else {
+    print("Keep working...")
+}
