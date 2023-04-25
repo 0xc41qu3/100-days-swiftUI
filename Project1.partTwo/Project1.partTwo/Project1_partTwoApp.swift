0e0f2f14736b84799de69324a6bc4712fb53e018
Caique Meira Oliveira <caique_m_oliveira@icloud.com>
2023-04-24 21:56:30 -0300

    create second part of the first project

---
+++ Project1.partTwo/Project1.partTwo/Project1_partTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project1_partTwoApp.swift
+//  Project1.partTwo
+//
+//  Created by Caique Meira Oliveira on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Project1_partTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
